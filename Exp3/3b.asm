473d712c45ed778ea39aff0348b05c3f45b1c374
D-Justin-Dsouza <justindsouza80@gmail.com>
2025-01-25 08:20:41 +0530

    File structure changed ,new programs(error)

---
+++ Exp3/3b.asm
@@ -0,0 +1,119 @@
+section .data
+    msg db "Enter two numbers: ", 0
+    sum_msg db "The sum is: ", 0
+    sub_msg db "The subtraction is: ",0
+    mul_msg db "Multiplication: ",0
+    endl db 10, 0
+
+section .bss
+    num1 resb 4
+    num2 resb 4
+    result resb 4
+
+section .text
+    global _start
+
+_start:
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, msg
+    mov edx, 20
+    int 80h
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num1
+    mov edx, 4
+    int 80h
+
+    mov eax, 3
+    mov ebx, 0
+    mov ecx, num2
+    mov edx, 4
+    int 80h
+
+    mov eax, [num1]
+    sub eax, '0'
+    mov ebx, [num2]
+    sub ebx, '0'
+
+    add eax, ebx
+
+    add eax, '0'
+    mov [result], eax
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, sum_msg
+    mov edx, 13
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 1
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, endl
+    mov edx, 1
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, sub_msg
+    mov edx, 21
+    int 80h
+
+    mov eax, [num1]
+    mov ebx, [num2]
+    sub eax, '0'
+    sub ebx, '0'
+
+    sub eax,ebx
+    add eax,'0'
+    
+    mov [result],eax
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 4
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, endl
+    mov edx, 1
+    int 80h
+
+    mov eax, [num1]
+    sub eax, '0'
+    mov ebx, [num2]
+    sub ebx, '0'
+    
+    mul ebx
+    add eax, '0'
+    mov [result], eax
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, mul_msg
+    mov edx, 16
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, result
+    mov edx, 1
+    int 80h
+
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, endl
+    mov edx, 1
+    int 80h
+    
+    mov eax, 1
+    mov ebx, 0
+    int 80h
+
